40ee47a8e886c5233c8d3a0adfa361e2a75b3d80
Juan <90655302+juank6883y@users.noreply.github.com>
2025-09-03 17:28:34 -0500

    Update procesar_formulario.php

--- paginas web/Huerta OFIR Fontibón/procesar_formulario.php
+++ paginas web/Huerta OFIR Fontibón/procesar_formulario.php
@@ -29,7 +29,7 @@ try {
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'taskpro.help7008@gmail.com';
+    $mail->Username   = #;
     $mail->Password   = #;
     $mail->SMTPSecure = 'tls';
     $mail->Port       = 587;
